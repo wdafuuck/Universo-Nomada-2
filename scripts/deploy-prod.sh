89e78de5ba781f061ec76fc3e26df52b37c8756d
Ricardo Segura <admaconcagua.hq@gmail.com>
2026-07-21 20:41:44 -0400

    Exclude production docker-compose from deploy sync after secret rotation.

--- scripts/deploy-prod.sh
+++ scripts/deploy-prod.sh
@@ -26,6 +26,7 @@ rsync -az --delete \
   --exclude '.cursor/' \
   --exclude 'dev.log' \
   --exclude 'server.log' \
+  --exclude 'docker-compose.yml' \
   -e "ssh ${SSH_OPTS[*]}" \
   "$ROOT/" "${USER}@${HOST}:${REMOTE}/"
 
